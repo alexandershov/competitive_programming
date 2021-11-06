33e11dc696c90e34c00ec72dc3c0007dac587447
aershov <codumentary.com@gmail.com>
2021-11-06 11:32:48 +0300

    implement recursive factorial_modulo

--- src/competitive_programming/chapter_2.py
+++ src/competitive_programming/chapter_2.py
@@ -1,1 +1,6 @@
-    
+def factorial_modulo(n: int, m: int) -> int:
+    assert n >= 0
+    if n == 0:
+        return 1
+
+    return ((n % m) * factorial_modulo(n - 1, m) % m) % m
--- tests/test_chapter_2.py
+++ tests/test_chapter_2.py
@@ -4,6 +4,7 @@ import pytest
 
 
 @pytest.mark.parametrize('n, modulo, expected', [
+    (5, 121, 120),
 ])
 def test_factorial_modulo(n, modulo, expected):
     assert chapter_2.factorial_modulo(n, modulo) == expected
