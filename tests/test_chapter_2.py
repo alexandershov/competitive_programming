f97be2089a4b34179a4f13cfd1fcde894d19dca0
aershov <codumentary.com@gmail.com>
2021-11-06 11:38:05 +0300

    fail with recursion exceeded

--- tests/test_chapter_2.py
+++ tests/test_chapter_2.py
@@ -7,6 +7,7 @@ from competitive_programming import chapter_2
 @pytest.mark.parametrize('n, modulo', [
     (5, 121),
     (5, 7),
+    (2000, 23422342),
 ])
 def test_factorial_modulo(n, modulo):
     expected = math.factorial(n) % modulo
