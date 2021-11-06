de9b05e1ab9b471e8a9ae8611917f19fe0560c3b
aershov <codumentary.com@gmail.com>
2021-11-06 11:34:10 +0300

    add test where modulo affects result

--- tests/test_chapter_2.py
+++ tests/test_chapter_2.py
@@ -5,6 +5,7 @@ import pytest
 
 @pytest.mark.parametrize('n, modulo, expected', [
     (5, 121, 120),
+    (5, 7, 1),
 ])
 def test_factorial_modulo(n, modulo, expected):
     assert chapter_2.factorial_modulo(n, modulo) == expected
