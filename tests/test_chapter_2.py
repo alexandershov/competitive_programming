d8ba04a4be94b991de1a343ce5d8c73c0f74b8a6
aershov <codumentary.com@gmail.com>
2021-11-06 11:36:54 +0300

    calculate expected factorial_modulo

--- tests/test_chapter_2.py
+++ tests/test_chapter_2.py
@@ -1,11 +1,13 @@
-from competitive_programming import chapter_2
+import math
 
 import pytest
+from competitive_programming import chapter_2
 
 
-@pytest.mark.parametrize('n, modulo, expected', [
-    (5, 121, 120),
-    (5, 7, 1),
+@pytest.mark.parametrize('n, modulo', [
+    (5, 121),
+    (5, 7),
 ])
-def test_factorial_modulo(n, modulo, expected):
+def test_factorial_modulo(n, modulo):
+    expected = math.factorial(n) % modulo
     assert chapter_2.factorial_modulo(n, modulo) == expected
