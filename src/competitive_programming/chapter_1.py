2d378c41f11512fba49f96e12936aa61d5b7f95a
aershov <codumentary.com@gmail.com>
2021-11-06 08:00:04 +0300

    get rid of floating points

--- src/competitive_programming/chapter_1.py
+++ src/competitive_programming/chapter_1.py
@@ -1,14 +1,12 @@
 def weird_algorithm(n: int) -> list[int]:
     assert n > 0
-    result = []
+    result = [n]
 
     while n != 1:
-        result.append(n)
-
         if n % 2:
             n = 3 * n + 1
         else:
-            n /= 2
+            n //= 2
+        result.append(n)
 
-    result.append(n)
     return result
