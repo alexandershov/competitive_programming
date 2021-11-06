06306535833369ea2e6cd6d24c4f6f205718ae99
aershov <codumentary.com@gmail.com>
2021-11-06 12:18:53 +0300

    make iterative solution

--- src/competitive_programming/chapter_2.py
+++ src/competitive_programming/chapter_2.py
@@ -1,6 +1,6 @@
 def factorial_modulo(n: int, m: int) -> int:
     assert n >= 0
-    if n == 0:
-        return 1
-
-    return ((n % m) * factorial_modulo(n - 1, m)) % m
+    result = 1
+    for i in range(1, n + 1):
+        result = ((i % m) * (result % m)) % m
+    return result
