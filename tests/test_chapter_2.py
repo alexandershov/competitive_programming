13d89ec647c2a92fe4969b5cf1f6c9077326ed03
aershov <codumentary.com@gmail.com>
2021-11-06 11:26:40 +0300

    add empty test for factorial modulo

--- tests/test_chapter_2.py
+++ tests/test_chapter_2.py
@@ -1,1 +1,9 @@
 from competitive_programming import chapter_2
+
+import pytest
+
+
+@pytest.mark.parametrize('n, modulo, expected', [
+])
+def test_factorial_modulo(n, modulo, expected):
+    assert chapter_2.factorial_modulo(n, modulo) == expected
