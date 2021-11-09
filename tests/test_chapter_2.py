fd1e2831b34b9678ffdceb50a1cf0b18774000c9
aershov <codumentary.com@gmail.com>
2021-11-09 10:23:48 +0300

    add test for subsets

--- tests/test_chapter_2.py
+++ tests/test_chapter_2.py
@@ -13,3 +13,21 @@ from competitive_programming import chapter_2
 def test_factorial_modulo(n, modulo):
     expected = math.factorial(n) % modulo
     assert chapter_2.factorial_modulo(n, modulo) == expected
+
+
+@pytest.mark.parametrize('seq, expected', [
+])
+def test_subsets(seq, expected):
+    actual = chapter_2.iter_subsets(seq)
+    assert_same_subsets(actual, expected)
+
+
+def assert_same_subsets(actual, expected):
+    return normalized_subsets(actual) == normalized_subsets(expected)
+
+
+def normalized_subsets(subsets):
+    return sorted(
+        sorted(subset)
+        for subset in subsets
+    )
