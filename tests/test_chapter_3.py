5842f1e3fc6eb15fe546e8f51eaf18b3750a4ab1
aershov <codumentary.com@gmail.com>
2021-12-03 09:11:24 +0300

    more tests for binary function

--- tests/test_chapter_3.py
+++ tests/test_chapter_3.py
@@ -6,6 +6,7 @@ import pytest
 @pytest.mark.parametrize('function, n, expected', [
     (chapter_3.binary_function, 1, 1),
     (chapter_3.binary_function, 2, 3),
+    (chapter_3.binary_function, 3, 7),
 ])
 def test_recursive_num_calls(function, n, expected):
     actual = len(list(function(n)))
