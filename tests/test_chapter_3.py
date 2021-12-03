d125ae042e318dce9afca4574434c0f8af16c4ee
aershov <codumentary.com@gmail.com>
2021-12-03 09:14:19 +0300

    test binary function for a larger number

--- tests/test_chapter_3.py
+++ tests/test_chapter_3.py
@@ -7,6 +7,7 @@ import pytest
     (chapter_3.binary_function, 1, 1),
     (chapter_3.binary_function, 2, 3),
     (chapter_3.binary_function, 3, 7),
+    (chapter_3.binary_function, 10, 1023),
 ])
 def test_recursive_num_calls(function, n, expected):
     actual = len(list(function(n)))
