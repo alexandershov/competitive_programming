493de0d0a207587e87549d3d4922208c024bd1d5
aershov <codumentary.com@gmail.com>
2021-12-04 13:17:06 +0300

    add test scaffold for get_max_subarray

--- tests/test_chapter_3.py
+++ tests/test_chapter_3.py
@@ -15,3 +15,10 @@ import pytest
 def test_recursive_num_calls(n, x, expected):
     actual = len(list(chapter_3.n_ary_function(n, x)))
     assert actual == expected
+
+
+@pytest.mark.parametrize('seq, expected', [
+])
+def test_get_max_subarray_sum(seq, expected):
+    actual = chapter_3.get_max_subarray_sum_brute_force(seq)
+    assert actual == expected
