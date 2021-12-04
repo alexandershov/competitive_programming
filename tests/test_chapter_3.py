b8c71c79e2d09161330481ca9cb6b3ba73c3dbac
aershov <codumentary.com@gmail.com>
2021-12-04 10:30:43 +0300

    check ternary function

--- tests/test_chapter_3.py
+++ tests/test_chapter_3.py
@@ -4,10 +4,12 @@ import pytest
 
 
 @pytest.mark.parametrize('n, x, expected', [
+    # num_calls = (n^x - 1) / (n - 1)
     (2, 1, 1),
     (2, 2, 3),
     (2, 3, 7),
     (2, 10, 1023),
+    (3, 3, 13)
 ])
 def test_recursive_num_calls(n, x, expected):
     actual = len(list(chapter_3.n_ary_function(n, x)))
