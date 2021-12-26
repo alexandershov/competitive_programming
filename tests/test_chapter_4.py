c9f5e91728f14f86b4dfc5ecd2073261a9595d26
aershov <codumentary.com@gmail.com>
2021-12-26 13:43:06 +0300

    add already sorted case for test sorting

--- tests/test_chapter_4.py
+++ tests/test_chapter_4.py
@@ -13,6 +13,11 @@ from competitive_programming import chapter_4
         [],
         id='should work on empty input'
     ),
+    pytest.param(
+        [1, 2, 3],
+        [1, 2, 3],
+        id='should leave sorted input unchanged'
+    )
 ])
 def test_sorting(sorting_algo, seq, expected):
     copy = seq.copy()
