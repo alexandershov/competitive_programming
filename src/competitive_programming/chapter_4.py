5d4cff08b3dab80e2a08a57393fd90da862c86a2
aershov <codumentary.com@gmail.com>
2021-12-26 13:49:21 +0300

    optimize bubble sort

--- src/competitive_programming/chapter_4.py
+++ src/competitive_programming/chapter_4.py
@@ -1,9 +1,8 @@
 def bubble_sort(seq: list):
-    for _ in range(len(seq)):
-        for i in range(0, len(seq) - 1):
-            j = i + 1
-            if seq[i] > seq[j]:
-                swap(seq, i, j)
+    for i in range(len(seq)):
+        for j in range(0, len(seq) - i - 1):
+            if seq[j] > seq[j + 1]:
+                swap(seq, j, j + 1)
 
 
 def swap(seq, i, j):
