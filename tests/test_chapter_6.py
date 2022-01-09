533ad318e459f91a118a21e4292b6ed0628178ee
aershov <codumentary.com@gmail.com>
2022-01-09 13:48:23 +0300

    solve coins problem

--- tests/test_chapter_6.py
+++ tests/test_chapter_6.py
@@ -4,6 +4,13 @@ from competitive_programming import chapter_6
 
 
 @pytest.mark.parametrize('coins, amount, expected', [
+    # TODO: what if there's no solution?, what if solution is empty?
+    pytest.param(
+        {100, 99, 6, 1},
+        105,
+        [99, 6],
+        id='it should pick the minimum amount of coins'
+    )
 ])
 def test_solve_coins_problem(coins, amount, expected):
     assert sorted(chapter_6.solve_coins_problem(coins, amount)) == sorted(expected)
