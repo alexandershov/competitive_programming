a58b725f29ff3921eee64c3fc20f2e3e0053f5e8
aershov <codumentary.com@gmail.com>
2022-01-09 13:53:24 +0300

    handle empty solution

--- src/competitive_programming/chapter_6.py
+++ src/competitive_programming/chapter_6.py
@@ -3,14 +3,11 @@ def solve_coins_problem(coins: set[int], amount: int) -> list[int]:
     for a_coin in coins:
         assert a_coin > 0
 
-    cache = {}  # amount -> solution
+    cache = {0: []}  # amount -> solution
     for cur_amount in range(0, amount + 1):
-        if cur_amount in coins:
-            cache[cur_amount] = [cur_amount]
-        else:
-            for a_coin in coins:
-                prev_solution = cache.get(cur_amount - a_coin)
-                if prev_solution is not None:
-                    if cur_amount not in cache or len(cache[cur_amount]) > len(prev_solution) + 1:
-                        cache[cur_amount] = prev_solution + [a_coin]
+        for a_coin in coins:
+            prev_solution = cache.get(cur_amount - a_coin)
+            if prev_solution is not None:
+                if cur_amount not in cache or len(cache[cur_amount]) > len(prev_solution) + 1:
+                    cache[cur_amount] = prev_solution + [a_coin]
     return cache.get(amount)
--- tests/test_chapter_6.py
+++ tests/test_chapter_6.py
@@ -4,7 +4,6 @@ from competitive_programming import chapter_6
 
 
 @pytest.mark.parametrize('coins, amount, expected', [
-    # TODO: what if there's no solution?, what if solution is empty?
     pytest.param(
         {100, 99, 6, 1},
         105,
@@ -12,12 +11,17 @@ from competitive_programming import chapter_6
         id='it should pick the minimum amount of coins'
     ),
     pytest.param(
+        {100, 99, 6, 1},
+        0,
+        [],
+        id='it should return empty list if amount is zero'
+    ),
+    pytest.param(
         {100, 99, 6},
         10,
         None,
         id='it should return None if there\'s no solution'
     )
-
 ])
 def test_solve_coins_problem(coins, amount, expected):
     assert try_sorted(chapter_6.solve_coins_problem(coins, amount)) == try_sorted(expected)
