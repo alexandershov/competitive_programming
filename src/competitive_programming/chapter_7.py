e2713a67437fa02d125882cab660af54bb5776e8
aershov <codumentary.com@gmail.com>
2022-01-21 21:49:49 +0300

    implement bfs

--- src/competitive_programming/chapter_7.py
+++ src/competitive_programming/chapter_7.py
@@ -1,1 +1,12 @@
-    
+# graph algorithms
+
+def dfs(graph, starting_node):
+    frontier = [starting_node]
+    visited = set()
+    while frontier:
+        node = frontier.pop()
+        yield node
+        visited.add(node)
+        for neighbour in graph.get(node, []):
+            if neighbour not in visited:
+                frontier.append(neighbour)
--- tests/test_chapter_7.py
+++ tests/test_chapter_7.py
@@ -1,1 +1,12 @@
-    
+from competitive_programming import chapter_7
+
+
+def dfs():
+    graph = {
+        1: [8, 9],
+        9: [10, 11],
+        11: [2, 1],
+    }
+
+    nodes = list(chapter_7.dfs(graph, 1))
+    assert nodes == [1, 9, 11, 2, 10, 8]
