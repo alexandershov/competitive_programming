370bcef2d3f2fc5b1a9dd45e76724a1370cbb375
aershov <codumentary.com@gmail.com>
2022-01-22 08:04:52 +0300

    extract to search_algo

--- tests/test_chapter_7.py
+++ tests/test_chapter_7.py
@@ -1,12 +1,19 @@
+import pytest
+
 from competitive_programming import chapter_7
 
 
-def test_dfs():
+def test_dfs(search_algo):
     graph = {
         1: [8, 9],
         9: [10, 11],
         11: [2, 1],
     }
 
-    nodes = list(chapter_7.dfs(graph, 1))
+    nodes = list(search_algo(graph, 1))
     assert nodes == [1, 9, 11, 2, 10, 8]
+
+
+@pytest.fixture(params=[chapter_7.dfs])
+def search_algo(request):
+    return request.param
