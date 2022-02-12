6c28c97766be3aabf7df889bae8f89df5972d03f
aershov <codumentary.com@gmail.com>
2022-02-12 14:02:33 +0300

    implement get_range_sum

--- src/competitive_programming/chapter_9.py
+++ src/competitive_programming/chapter_9.py
@@ -1,2 +1,13 @@
 def get_range_sum(seq: list, first: int, last: int) -> int:
-    pass
+    assert last >= first
+    prefix_sums = get_prefix_sums(seq)
+    return prefix_sums[last] - prefix_sums[first] + seq[first]
+
+
+def get_prefix_sums(seq):
+    cur_sum = 0
+    sums = []
+    for item in seq:
+        cur_sum += item
+        sums.append(cur_sum)
+    return sums
--- tests/test_chapter_9.py
+++ tests/test_chapter_9.py
@@ -4,6 +4,11 @@ import pytest
 
 
 @pytest.mark.parametrize('seq, first, last, expected', [
+    pytest.param(
+        [8, 10, 12, 6, 7], 1, 3,
+        28,
+        id='it should return sum of elements in the range [first; last]'
+    )
 ])
 def test_get_range_sum(seq, first, last, expected):
     assert chapter_9.get_range_sum(seq, first, last) == expected
