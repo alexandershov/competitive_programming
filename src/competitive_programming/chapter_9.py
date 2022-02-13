d61850fb6c662de4eebbe032250a73de015f7028
aershov <codumentary.com@gmail.com>
2022-02-13 12:54:05 +0300

    implement get_range_min

--- src/competitive_programming/chapter_9.py
+++ src/competitive_programming/chapter_9.py
@@ -14,4 +14,31 @@ def get_prefix_sums(seq):
 
 
 def get_range_min(seq: list, first: int, last: int) -> int:
-    pass
+    assert first <= last
+    sparse_table = get_sparse_table(seq)
+    range_min = seq[first]
+    cur = last
+    for i, bit in enumerate(reversed(bin(last - first + 1)[2:])):
+        next_ = cur - int(bit) * (2 ** i)
+        if next_ != cur:
+            range_min = min(range_min, sparse_table[2 ** i][next_ + 1])
+            cur = next_
+    return range_min
+
+
+def get_sparse_table(seq: list):
+    table = {}
+    range_len = 1
+    while range_len <= len(seq):
+        row = {}
+        if range_len == 1:
+            for i, item in enumerate(seq):
+                row[i] = item
+        else:
+            for i, item in enumerate(seq):
+                if i + range_len <= len(seq):
+                    prev = table[range_len // 2]
+                    row[i] = min(prev[i], prev[i + range_len // 2])
+        table[range_len] = row
+        range_len *= 2
+    return table
--- tests/test_chapter_9.py
+++ tests/test_chapter_9.py
@@ -20,6 +20,11 @@ def test_get_range_sum(seq, first, last, expected):
 
 
 @pytest.mark.parametrize('seq, first, last, expected', [
+    pytest.param(
+        [8, 10, 12, 6, 7], 1, 3,
+        6,
+        id='it should return the minimum value in range'
+    )
 ])
 def test_get_range_min(seq, first, last, expected):
     assert chapter_9.get_range_min(seq, first, last) == expected
