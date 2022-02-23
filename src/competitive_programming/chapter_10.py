dd1e6ff2b6e461915234041ba0f5ca978a0b5aa2
aershov <codumentary.com@gmail.com>
2022-02-23 19:02:52 +0300

    implement tree_dfs

--- src/competitive_programming/chapter_10.py
+++ src/competitive_programming/chapter_10.py
@@ -1,2 +1,9 @@
-def tree_dfs(tree, initial_node):
-    pass
+def tree_dfs(tree, initial_node, prev_node=None):
+    yield initial_node
+    for node in get_adjacent_nodes(tree, initial_node):
+        if node != prev_node:
+            yield from tree_dfs(tree, node, initial_node)
+
+
+def get_adjacent_nodes(tree, node):
+    return tree.get(node, [])
--- tests/test_chapter_10.py
+++ tests/test_chapter_10.py
@@ -4,6 +4,20 @@ import pytest
 
 
 @pytest.mark.parametrize('tree, initial_node, expected', [
+    pytest.param(
+        {
+            'A': ['C', 'D'],
+            'B': ['D'],
+            'C': ['A', 'E'],
+            'D': ['A', 'B', 'F', 'G'],
+            'E': ['C'],
+            'F': ['D'],
+            'G': ['D'],
+        },
+        'D',
+        {'A', 'B', 'C', 'D', 'E', 'F', 'G'},
+        id='it should traverse all nodes in a tree',
+    )
 ])
 def test_tree_dfs(tree, initial_node, expected):
     nodes = set(chapter_10.tree_dfs(tree, initial_node))
