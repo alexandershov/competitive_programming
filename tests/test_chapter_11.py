25ceaa92dc3d030e857c401b4748e51ed4097225
aershov <codumentary.com@gmail.com>
2022-03-08 13:16:54 +0300

    add test for large not prime number

--- tests/test_chapter_11.py
+++ tests/test_chapter_11.py
@@ -8,6 +8,7 @@ from competitive_programming import chapter_11
     pytest.param(47, True, id='it should return True for 47'),
     pytest.param(16, False, id='it should return False for 16'),
     pytest.param(234234811, True, id='it should return True for 234234811'),
+    pytest.param(23497 * 23581, False, id='it should return True for 23497 * 23581'),
 ])
 def test_is_prime(number, expected):
     assert chapter_11.is_prime(number) is expected
