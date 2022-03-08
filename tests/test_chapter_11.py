0545c46684700b8722d7f7ccaa00b50b867c08cf
aershov <codumentary.com@gmail.com>
2022-03-08 13:12:25 +0300

    add test for larger prime

--- tests/test_chapter_11.py
+++ tests/test_chapter_11.py
@@ -7,6 +7,7 @@ from competitive_programming import chapter_11
     pytest.param(2, True, id='it should return True for 2'),
     pytest.param(47, True, id='it should return True for 47'),
     pytest.param(16, False, id='it should return False for 16'),
+    pytest.param(7919, True, id='it should return True for 7919'),
 ])
 def test_is_prime(number, expected):
     assert chapter_11.is_prime(number) is expected
