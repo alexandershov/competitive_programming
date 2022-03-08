528893ffc51d5ed1568ef166399ad7dd73f44061
aershov <codumentary.com@gmail.com>
2022-03-08 13:11:25 +0300

    add test when is_prime is False

--- tests/test_chapter_11.py
+++ tests/test_chapter_11.py
@@ -6,6 +6,7 @@ from competitive_programming import chapter_11
 @pytest.mark.parametrize('number, expected', [
     pytest.param(2, True, id='it should return True for 2'),
     pytest.param(47, True, id='it should return True for 47'),
+    pytest.param(16, False, id='it should return False for 16'),
 ])
 def test_is_prime(number, expected):
     assert chapter_11.is_prime(number) is expected
