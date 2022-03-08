e5acc294bf7d404b4fe9df1eedc53e4f71a6db57
aershov <codumentary.com@gmail.com>
2022-03-08 13:11:03 +0300

    add test for primality

--- tests/test_chapter_11.py
+++ tests/test_chapter_11.py
@@ -5,6 +5,7 @@ from competitive_programming import chapter_11
 
 @pytest.mark.parametrize('number, expected', [
     pytest.param(2, True, id='it should return True for 2'),
+    pytest.param(47, True, id='it should return True for 47'),
 ])
 def test_is_prime(number, expected):
     assert chapter_11.is_prime(number) is expected
