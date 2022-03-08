7c2e0b3d76350c6e3593163f00669b194c070aac
aershov <codumentary.com@gmail.com>
2022-03-08 13:19:24 +0300

    add prime tests for 0 & 1

--- src/competitive_programming/chapter_11.py
+++ src/competitive_programming/chapter_11.py
@@ -1,4 +1,6 @@
 def is_prime(number: int) -> bool:
+    if number < 2:
+        return False
     factor = 2
     while factor ** 2 <= number:
         if number % factor == 0:
--- tests/test_chapter_11.py
+++ tests/test_chapter_11.py
@@ -4,6 +4,8 @@ from competitive_programming import chapter_11
 
 
 @pytest.mark.parametrize('number, expected', [
+    pytest.param(0, False, id='it should return False for 0'),
+    pytest.param(1, False, id='it should return False for 1'),
     pytest.param(2, True, id='it should return True for 2'),
     pytest.param(3, True, id='it should return True for 3'),
     pytest.param(4, False, id='it should return False for 4'),
