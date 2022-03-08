fd61400f038bdc13a1983ba8d33d8cd70dc1fc93
aershov <codumentary.com@gmail.com>
2022-03-08 13:16:03 +0300

    more efficient is_prime

--- src/competitive_programming/chapter_11.py
+++ src/competitive_programming/chapter_11.py
@@ -1,5 +1,7 @@
 def is_prime(number: int) -> bool:
-    for factor in range(2, number):
+    factor = 2
+    while factor ** 2 <= number:
         if number % factor == 0:
             return False
+        factor += 1
     return True
