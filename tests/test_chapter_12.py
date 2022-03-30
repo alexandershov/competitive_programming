38b9ede226137bf76c9e5f9102b4a25bb85722da
aershov <codumentary.com@gmail.com>
2022-03-30 10:30:21 +0300

    test empty graph

--- tests/test_chapter_12.py
+++ tests/test_chapter_12.py
@@ -13,7 +13,12 @@ from competitive_programming import chapter_12
         },
         {frozenset('A'), frozenset('BCD')},
         id='it should return strongly connected components of a graph'
-    )
+    ),
+    pytest.param(
+        {},
+        set(),
+        id='it should return an empty set if graph is empty'
+    ),
 ])
 def test_get_strongly_connected_components(graph, expected):
     assert chapter_12.get_strongly_connected_components(graph) == expected
