64bb2d8231892770f697a995ef32fc8a0c6bc922
aershov <codumentary.com@gmail.com>
2022-03-30 10:28:26 +0300

    implement get_strongly_connected_components

--- src/competitive_programming/chapter_12.py
+++ src/competitive_programming/chapter_12.py
@@ -1,2 +1,28 @@
+import collections
+
+
 def get_strongly_connected_components(graph):
-    pass
+    reachable_by_node = {}
+    for node in graph:
+        reachable_by_node[node] = get_reachable_nodes(graph, node)
+    nodes_by_reachable = collections.defaultdict(set)
+    for node, reachable in reachable_by_node.items():
+        nodes_by_reachable[frozenset(reachable)].add(node)
+    return set(map(frozenset, nodes_by_reachable.values()))
+
+
+def get_reachable_nodes(graph, node):
+    visited = set()
+    frontier = [node]
+    while frontier:
+        cur = frontier.pop()
+        if cur in visited:
+            continue
+        visited.add(cur)
+        for neighbour in get_neighbours(graph, cur):
+            frontier.append(neighbour)
+    return visited
+
+
+def get_neighbours(graph, node):
+    return graph.get(node, [])
--- tests/test_chapter_12.py
+++ tests/test_chapter_12.py
@@ -4,6 +4,16 @@ from competitive_programming import chapter_12
 
 
 @pytest.mark.parametrize('graph, expected', [
+    pytest.param(
+        {
+            'A': ['B'],
+            'B': ['C'],
+            'C': ['D'],
+            'D': ['B']
+        },
+        {frozenset('A'), frozenset('BCD')},
+        id='it should return strongly connected components of a graph'
+    )
 ])
 def test_get_strongly_connected_components(graph, expected):
     assert chapter_12.get_strongly_connected_components(graph) == expected
