ec03222eac4f506f5571cc3fdaf034b4ded32806
aershov <codumentary.com@gmail.com>
2022-03-30 10:33:15 +0300

    test on an example from the book

--- tests/test_chapter_12.py
+++ tests/test_chapter_12.py
@@ -15,6 +15,19 @@ from competitive_programming import chapter_12
         id='it should return strongly connected components of a graph'
     ),
     pytest.param(
+        {
+            '1': ['2', '4'],
+            '2': ['1', '5'],
+            '3': ['2', '7'],
+            '4': [],
+            '5': ['4'],
+            '6': ['3', '5'],
+            '7': ['6'],
+        },
+        {frozenset('12'), frozenset('4'), frozenset('5'), frozenset('367')},
+        id='it should work on an example from the book'
+    ),
+    pytest.param(
         {},
         set(),
         id='it should return an empty set if graph is empty'
