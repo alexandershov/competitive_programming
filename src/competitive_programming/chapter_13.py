4b003c589853e694338b25e6607661e523db59c0
aershov <codumentary.com@gmail.com>
2022-04-14 10:31:50 +0300

    add test stub for get_point_side

--- src/competitive_programming/chapter_13.py
+++ src/competitive_programming/chapter_13.py
@@ -1,1 +1,17 @@
-    
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Point:
+    x: float
+    y: float
+
+
+@dataclass(frozen=True)
+class Line:
+    a: Point
+    b: Point
+
+
+def get_point_side(line: Line, point: Point) -> str:
+    return None
--- tests/test_chapter_13.py
+++ tests/test_chapter_13.py
@@ -1,1 +1,9 @@
-    
+import pytest
+
+from competitive_programming import chapter_13
+
+
+@pytest.mark.parametrize('line, point, expected', [
+])
+def test_get_point_side(line, point, expected):
+    assert chapter_13.get_point_side(line, point) == expected
