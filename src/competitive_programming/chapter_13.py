ac74a70ace1257dcaa5a861407fb2a179b10fa23
aershov <codumentary.com@gmail.com>
2022-04-14 10:39:54 +0300

    implement get_point_side

--- src/competitive_programming/chapter_13.py
+++ src/competitive_programming/chapter_13.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 
@@ -6,12 +8,29 @@ class Point:
     x: float
     y: float
 
+    def __mul__(self, other: Point) -> float:
+        return self.x * other.y - other.x * self.y
+
+    def __sub__(self, other: Point) -> Point:
+        return Point(
+            self.x - other.x,
+            self.y - other.y,
+        )
+
 
 @dataclass(frozen=True)
 class Line:
+    # TODO: rename a & b
     a: Point
     b: Point
 
 
 def get_point_side(line: Line, point: Point) -> str:
-    return None
+    first = line.b - line.a
+    second = point - line.b
+    cross_product = first * second
+    if cross_product > 0:
+        return 'left'
+    elif cross_product < 0:
+        return 'right'
+    return 'center'
--- tests/test_chapter_13.py
+++ tests/test_chapter_13.py
@@ -1,9 +1,16 @@
 import pytest
 
 from competitive_programming import chapter_13
+from competitive_programming.chapter_13 import Point
+from competitive_programming.chapter_13 import Line
 
 
 @pytest.mark.parametrize('line, point, expected', [
+    pytest.param(
+        Line(Point(1, 1), Point(3, 3)), Point(2, 3),
+        'left',
+        id='it should return "left" when point is located on the left side of the line'
+    )
 ])
 def test_get_point_side(line, point, expected):
     assert chapter_13.get_point_side(line, point) == expected
