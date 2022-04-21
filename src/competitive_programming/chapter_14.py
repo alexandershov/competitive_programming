0399fbcd1b2c433c84900c496c6a81a3a607abcb
aershov <codumentary.com@gmail.com>
2022-04-21 11:03:53 +0300

    add test stub for find_longest_common_subsequence

--- src/competitive_programming/chapter_14.py
+++ src/competitive_programming/chapter_14.py
@@ -1,1 +1,2 @@
-    
+def find_longest_common_subsequence(left: str, right: str) -> str:
+    pass
--- tests/test_chapter_14.py
+++ tests/test_chapter_14.py
@@ -1,1 +1,9 @@
-    
+import pytest
+
+from competitive_programming import chapter_14
+
+
+@pytest.mark.parametrize('left, right, expected', [
+])
+def test_find_longest_common_subsequence(left, right, expected):
+    assert chapter_14.find_longest_common_subsequence(left, right) == expected
