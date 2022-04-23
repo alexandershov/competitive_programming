c2288a7c71a51291a504db1dd9bfa71141877bbe
aershov <codumentary.com@gmail.com>
2022-04-23 15:47:29 +0300

    add caching

--- src/competitive_programming/chapter_14.py
+++ src/competitive_programming/chapter_14.py
@@ -1,9 +1,17 @@
-def find_longest_common_subsequence(left: str, right: str) -> str:
-    # TODO: add caching
+def find_longest_common_subsequence(left: str, right: str, cache=None) -> str:
+    if cache is None:
+        cache = {}
+
+    key = (left, right)
+    if key in cache:
+        return cache[key]
+
     if not left or not right:
         return ''
     if left[-1] == right[-1]:
-        return find_longest_common_subsequence(left[:-1], right[:-1]) + left[-1]
-    first = find_longest_common_subsequence(left[:-1], right)
-    second = find_longest_common_subsequence(left, right[:-1])
-    return max(first, second, key=len)
+        return find_longest_common_subsequence(left[:-1], right[:-1], cache) + left[-1]
+    first = find_longest_common_subsequence(left[:-1], right, cache)
+    second = find_longest_common_subsequence(left, right[:-1], cache)
+    result = max(first, second, key=len)
+    cache[key] = result
+    return result
