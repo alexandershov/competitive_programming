d1b3df2c0812d787dc73d7b399099829a9b42150
aershov <codumentary.com@gmail.com>
2022-04-23 15:45:05 +0300

    implement naive longest_common_subsequence

--- src/competitive_programming/chapter_14.py
+++ src/competitive_programming/chapter_14.py
@@ -1,2 +1,9 @@
 def find_longest_common_subsequence(left: str, right: str) -> str:
-    pass
+    # TODO: add caching
+    if not left or not right:
+        return ''
+    if left[-1] == right[-1]:
+        return find_longest_common_subsequence(left[:-1], right[:-1]) + left[-1]
+    first = find_longest_common_subsequence(left[:-1], right)
+    second = find_longest_common_subsequence(left, right[:-1])
+    return max(first, second, key=len)
--- tests/test_chapter_14.py
+++ tests/test_chapter_14.py
@@ -4,6 +4,11 @@ from competitive_programming import chapter_14
 
 
 @pytest.mark.parametrize('left, right, expected', [
+    pytest.param(
+        'tour', 'opera',
+        'or',
+        id='it should return longest common subsequence'
+    )
 ])
 def test_find_longest_common_subsequence(left, right, expected):
     assert chapter_14.find_longest_common_subsequence(left, right) == expected
